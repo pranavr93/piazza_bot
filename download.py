a43ebd398d8018c96e64278863ed94b98d5ccb38
Pranav Ramarao <pranav.rr93@gmail.com>
2017-03-16 11:49:30 -0400

    updated based on config file

--- download.py
+++ download.py
@@ -1,20 +1,21 @@
 from piazza_api import Piazza
 import json
 import sys
+import config
 
 p = Piazza()
-p.user_login()
+p.user_login(config.creds['email'], config.creds['password'])
 
-course = p.network(sys.argv[1])
+course = p.network(config.class_code)
 
 mapSave = {}
 
 posts = course.iter_all_posts(limit=100000000000)
 for post in posts:
-	content = post["history"][0]["content"]
-	id = post["nr"]
-	print (id)
-	mapSave[id] = content
+    content = post["history"][0]["content"]
+    id = post["nr"]
+    print(id)
+    mapSave[id] = content
 
-with open("posts183.json","wb") as f:
-	f.write(json.dumps(mapSave))
+with open("posts_test.json", "wb") as f:
+    f.write(json.dumps(mapSave))
