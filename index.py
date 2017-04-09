be4d51173ab700d66143e00bd4c034850bf9cd0d
Pranav Ramarao <pranav.rr93@gmail.com>
2017-04-09 11:30:02 -0400

    add date, numeric and term abstractions on search

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 from whoosh.index import create_in, open_dir
+from whoosh.qparser import QueryParser
 from whoosh.fields import *
+from whoosh.query import *
 import os.path
 import config
-from whoosh.qparser import QueryParser
+
 import sys
 from bot import Bot
 import shutil
-
+from dateutil import parser
 class Index:
     def __init__(self):
         # define schema for the index
@@ -27,13 +29,15 @@ class Index:
     def get_schema(self):
         # build index on post and subject; not storing them
         # store raw json in 'json' field
-        return Schema(post=TEXT, subject=TEXT, json=STORED)
+        return Schema(subject=TEXT, body=TEXT, date=DATETIME, json=STORED, views=NUMERIC)
 
     def post_to_document(self, document):
         return  { 
-                    'post': document['history'][0]['content'],
+                    'body': document['history'][0]['content'],
                     'subject': document['history'][0]['subject'],
-                    'json': document
+                    'date': parser.parse(document['created']),
+                    'json': document,
+                    'views': int(document['unique_views'])
                 } 
 
     # create index from scratch
@@ -43,7 +47,7 @@ class Index:
             dict = self.post_to_document(document)
             writer.add_document(**dict)
         writer.commit()
-        print('\n-----------index created-----------\n\n')
+        print('\nindex created!\n\n')
 
     # incremental update to index
     def update_index(self):
@@ -51,20 +55,34 @@ class Index:
 
     # search for query terms in the description
     def search(self, query):
-        print('searching \'{0}\'...'.format(query))
-        all_results = []
+        print(query)
+        ret_list = []
+        ret_list = []
         with self.ix.searcher() as searcher:
-            for word in query.split(' '):
-                query = QueryParser('post', self.ix.schema).parse(word)
-                results = searcher.search(query)
-                all_results.extend(results)
+            results = searcher.search(query)
+            for result in results:
+                ret_list.append(dict(result))
+        return ret_list
 
-            if len(results) > 0:
-                print('here are the matches:\n')
-                for result in all_results:
-                    print(result)
-            else:
-                print('no matches found\n')
+    # get all posts with field having query terms
+    def search_terms(self, field, query_terms):
+        print('searching \'{0}\'...'.format(query_terms))
+        all_results = []
+        for word in query_terms.split():
+            query = Term(field, word)
+            all_results.extend(self.search(query))
+        return all_results
+
+    # get all posts with field between low and high
+    def search_numeric(self, field, low, high):
+        query = NumericRange(field, low, up)
+        return self.search(query)
+
+    # get all posts between dates start_date and end_date
+    def search_date(self, field, start_date, end_date):
+        query = DateRange(field, start_date, end_date)
+        return self.search(query)
+
 
 def main():
     if len(sys.argv) == 1:
@@ -72,7 +90,14 @@ def main():
         return
     query_terms = sys.argv[1:]
     id = Index()
-    id.search(' '.join(query_terms))
+    results = id.search_terms('body',' '.join(query_terms))
+    print(len(results))
+    print(results[0])
+    for result in results:
+        print(result)
+        print('\n')
+    if len(results) == 0:
+        print('no search results for the given term')
 
 if __name__ == "__main__":
     main()
