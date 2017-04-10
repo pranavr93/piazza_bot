eb9bff5525b34bf90d4b535711fcd7c09d692825
Pranav Ramarao <pranav.rr93@gmail.com>
2017-04-09 21:45:55 -0400

    add create post and rpc piazza api support

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
+# bot that deals with interactions with piazza
+# sits in between Jarvis and Piazza
 from piazza_api import Piazza
 import json
 import sys
 import config
+from post import Post
+
+from piazza_api.rpc import PiazzaRPC
+
+
 
 class Bot:
     def __init__(self):
         self.piazza = Piazza()
         self.piazza.user_login(config.creds['email'], config.creds['password'])
         self.course = self.piazza.network(config.class_code)
+        # self.course = self.piazza.network(config.eecs281)
+
+        self.piazza_rpc = PiazzaRPC(config.class_code)
+        self.piazza_rpc.user_login(config.creds['email'], config.creds['password'])
 
     def get_all_posts(self):
         documents = []
-        posts = self.course.iter_all_posts()
+        posts = self.course.iter_all_posts(limit=100)
         for post in posts:
             print('downloading post {0}'.format(post['nr']))
-            documents.append(post)
+            documents.append(Post(post))
         return documents
+
+    def create_post(self, subject, body, folder=['hw1']):
+        params = {'type':'note','subject':subject, 'content':body, 'folders':folder}
+        self.piazza_rpc.content_create(params)
