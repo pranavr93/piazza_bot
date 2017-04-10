5a640266d25b09cac3fd59a3bcc1b78f34ccb157
Pranav Ramarao <pranav.rr93@gmail.com>
2017-04-09 21:46:21 -0400

    add eecs281 class code

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 creds = dict(
-	email = 'jarvisbot@umich.edu',
-	password = 'keyboard'
+    email = 'jarvisbot@umich.edu',
+    password = 'keyboard'
 )
 class_code = 'j0a1bog7vzu2cj'
+eecs281='ipgo72nw7yu1jv'
 index_directory = './index_dir/'
