177fda0bd2e77d16c29523ea1d7f024c8ef4db4c
yukepeter <yuke@umich.edu>
2017-04-11 05:04:29 +0000

    The function take input doc and return top n similar docs in document list

---
+++ topNdoc.py
@@ -0,0 +1,88 @@
+'''
+04/09/2017 Ke Yu
+
+Requirement:
+	nput_doc type is Post
+	documents is list[] of Post objects
+	n is the top 'n' similar documents
+
+Effect:
+	Given input_doc , return n most similar docs in <documents> 
+
+Reference:
+	http://stackoverflow.com/questions/12118720/python-tf-idf-cosine-to-find-document-similarity
+'''
+
+import numpy as np
+import re
+import nltk
+import string
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens, stemmer):
+	#credit to http://www.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+def tokenize(text):
+	#credit to http://www.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+def clean_docs(documents):
+	doc_clean = []
+	for i in documents:
+	    temp = re.sub("</?p[^>]*>", "", i)
+	    temp = temp.lower()
+	    tokens = temp.split(" ")
+	    token_list = []
+	    for w in tokens:
+	        w = re.sub(r'[^a-zA-Z]+', '', w)
+	        w = w.lower()
+	        if w not in stopwords.words('english') and w != "":
+	            token_list.append(w)
+	    doc_clean.append(token_list)
+
+	documents_clean = []
+	for i in doc_clean:
+	    documents_clean.append(" ".join(i))
+	return documents_clean 
+
+def find_top_N_similar_docs(input_doc, documents, n):
+	#Preprocess each document in list and build a new list of docs
+	'''
+	doc_clean = []
+	for i in documents:
+		temp = re.sub("</?p[^>]*>", "", i.body)
+		doc_clean.append(temp)
+	input_body = re.sub("</?p[^>]*>", "", input_doc.body)
+	'''
+	#clean the input_doc and documents--> input_doc_clean, doc_clean
+	documents.append(input_doc)
+	doc_clean = clean_docs(documents)
+	input_doc_clean = doc_clean[-1:]
+	#input_doc_clean.append(input_doc)
+	#doc_clean.extend(input_doc_clean)
+
+	tfidf = TfidfVectorizer().fit_transform(doc_clean)
+	input_vec = tfidf[-1:]
+
+	from sklearn.metrics.pairwise import linear_kernel
+
+	cosine_similarities = linear_kernel(input_vec, tfidf).flatten()
+	related_docs_indices = cosine_similarities.argsort()
+	if n != 0:
+		top_n_idx = related_docs_indices[0:n]
+		cosine_score = cosine_similarities[top_n_idx]
+	res = []
+	for i in top_n_idx:
+		res.append(documents[i])
+	return res[1:]
+	# Calculate TFIDF of all documents
